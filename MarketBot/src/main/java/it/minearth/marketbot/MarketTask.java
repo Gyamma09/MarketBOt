d74591ca1a60b8fc9c7d60f4280dd794d2eb4add
Gyamma09 <giammariamatteo029@gmail.com>
2026-04-05 21:57:22 +0200

    Update MarketTask.java

--- MarketBot/src/main/java/it/minearth/marketbot/MarketTask.java
+++ MarketBot/src/main/java/it/minearth/marketbot/MarketTask.java
@@ -167,8 +167,8 @@ public class MarketTask extends BukkitRunnable {
             String fieldValue =
                 "```diff\n"
                 + prefix + r.name + "  (" + String.format("%+.1f%%", r.pctSell) + ")\n"
-                + String.format("  Acq:  %.2f€ %s %+.2f€\n", r.buyPrice,  buyArrow,  r.diffBuy)
-                + String.format("  Vend: %.2f€ %s %+.2f€\n", r.sellPrice, sellArrow, r.diffSell)
+                + String.format("  B:%.2f€ %s%+.2f€\n", r.buyPrice,  buyArrow,  r.diffBuy)
+                + String.format("  S:%.2f€ %s%+.2f€\n", r.sellPrice, sellArrow, r.diffSell)
                 + "```";
 
             fields.append("{");
